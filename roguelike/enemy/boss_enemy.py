eab3419a7f6296c884891d9901aa576f2a1bc45f
Nikita Stepanov <nikita.stepanov@assaia.com>
2024-12-05 00:50:00 +0400

    added interfaces for enemies

---
+++ roguelike/enemy/boss_enemy.py
@@ -0,0 +1,37 @@
+from base_enemy import BaseEnemy
+
+
+class BossEnemy(BaseEnemy):
+    """BossEnemy logic."""
+
+    def move(self) -> None:
+        """Move logic.
+
+        Returns:
+            None: Description of return value
+        """
+        pass
+
+    def attack_player(self) -> None:
+        """attack_player logic.
+
+        Returns:
+            None: Description of return value
+        """
+        pass
+
+    def die(self) -> None:
+        """Die logic.
+
+        Returns:
+            None: Description of return value
+        """
+        pass
+
+    def unleash_ultimate(self) -> None:
+        """unleash_ultimate logic.
+
+        Returns:
+            None: Description of return value
+        """
+        pass
